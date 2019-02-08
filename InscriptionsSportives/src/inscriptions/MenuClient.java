073083c0acf09f3ed5b96be4b93beeb1f59ddd3a
Guillaume <g.rossinot@gmail.com>
2019-02-08 09:40:29 +0100

    modification menu client

--- InscriptionsSportives/src/inscriptions/MenuClient.java
+++ InscriptionsSportives/src/inscriptions/MenuClient.java
@@ -13,7 +13,7 @@ public class MenuClient {
      */
 
     public Menu menuHome() {
-        Menu menu = new Menu("Menu | Inscriptions Sportives");
+        Menu menu = new Menu("Menu Inscriptions Sportives");
         /* ajout du menu compétition */
         menu.add( CompetitionMenu() );
         /* ajout du menu équipe*/
@@ -32,12 +32,12 @@ public class MenuClient {
         Menu menu = new Menu("Quitter", "q");
         menu.add(saveAndQuit());
         menu.add(quitWhitoutSaving());
-        menu.addBack("b");
+        menu.addBack("x");
         return menu;
     }
     /* Sauvegarder et quitter le menu */
     private Option saveAndQuit() {
-        return new Option("Sauvegarder et Quitter", "s", new Action() {
+        return new Option("Save et Quitter", "s", new Action() {
             @Override
             public void optionSelected() {
                 try {
@@ -45,7 +45,7 @@ public class MenuClient {
                     Action.QUIT.optionSelected();
                 }
                 catch (Exception e) {
-                    System.out.println("Impossible de sauvegarder les données ! : " + e);
+                    System.out.println("Impossible de sauvegarder : " + e);
                 }
             }
         });
@@ -60,18 +60,18 @@ public class MenuClient {
     /* Menu pour les Competitions */
 
     private Menu CompetitionMenu() {
-        Menu menu = new Menu("Menu | Compétitions", "Compétition", "c");
-        menu.add( addCompetitionOption() );
-        menu.add( displayCompetitionOption() );
-        menu.add( manageCompetitionMenu() );
+        Menu menu = new Menu("Menu Competitions", "Competition", "a");
+        menu.add( addCompetition() );
+        menu.add( displayCompetition() );
+        menu.add( manageCompetition() );
 
-        menu.addBack("b");
+        menu.addBack("x");
         return menu;
     }
 
     /* Affichage competitions */
-    private Option displayCompetitionOption() { return new Option(
-            "Afficher les compétitions", "2",
+    private Option displayCompetition() { return new Option(
+            "Afficher competitions", "2",
             new Action() {
                 @Override
                 public void optionSelected() {
@@ -80,35 +80,35 @@ public class MenuClient {
             });
     }
     /* Ajout competitions */
-    private Option addCompetitionOption() {
+    private Option addCompetition() {
         return new Option(
-                "Ajouter une compétition", "1",
+                "Ajouter competition", "1",
                 new Action() {
                     @Override
                     public void optionSelected()
                     {
-                        String nomCompetition = InOut.getString("Nom de la compétition: ");
-                        System.out.println("Fin de la compétition :");
-                        int jour = InOut.getInt("  Jour (jj) : ");
-                        int mois = InOut.getInt("  Mois (mm) : ");
-                        int annee = InOut.getInt("  Année (yyyy) : ");
-                        int choice = InOut.getInt("La compétition se déroule t'elle en équipe ? (0 pour Non, 1 pour oui) ");
+                        String nomCompetition = InOut.getString("Nom competition: ");
+                        System.out.println("Fin competition :");
+                        int day = InOut.getInt("  Jour (jj) : ");
+                        int month = InOut.getInt("  Mois (mm) : ");
+                        int year = InOut.getInt("  Année (yyyy) : ");
+                        int choice = InOut.getInt("competition en equipe ? (2 pour Non, 1 pour oui) ");
                         boolean isTeam = false;
                         if(choice == 1){
                             isTeam = true;
-                        } else if(choice != 0 && choice != 1 ){
-                            System.out.println("Erreur de saisie, veuillez entrer 1 ou 0 ! ");
+                        } else if(choice != 2 && choice != 1 ){
+                            System.out.println("Erreur de saisie, veuillez entrer 1 ou 2 ! ");
                         }
-                        LocalDate dateCloture = LocalDate.of(annee, mois, jour);
-                        Inscriptions.getInscriptions().createCompetition(nomCompetition, dateCloture, isTeam);
+                        LocalDate dateClose = LocalDate.of(year, month, day);
+                        Inscriptions.getInscriptions().createCompetition(nomCompetition, dateClose, isTeam);
                     }
                 }
         );
     }
     /* Supprimer competitions */
-    private Option deleteCompetitionOption() {
+    private Option deleteCompetition() {
         return new List<>(
-                "Supprimer une compétition", "1",
+                "Supprimer competition", "1",
                 new ListData<Competition>()
                 {
                     @Override
@@ -127,17 +127,17 @@ public class MenuClient {
         );
     }
     /* Modifier competitions */
-    private Menu manageCompetitionMenu() {
-        Menu menu = new Menu("Menu | Gestion des compétitions", "Gestion des compétitions", "3");
-        menu.add( deleteCompetitionOption() );
-        menu.add( modifyCompetitionOption() );
-        menu.addBack("b");
+    private Menu manageCompetition() {
+        Menu menu = new Menu("Menu Gestion competitions", "Gestion competitions", "3");
+        menu.add( deleteCompetition() );
+        menu.add( modifyCompetition() );
+        menu.addBack("x");
         return menu;
     }
 
-    private Option modifyCompetitionOption() {
+    private Option modifyCompetition() {
         return new List<>(
-                "Modifier une compétition", "2",
+                "Modifier competition", "2",
                 new ListData<Competition>()
                 {
                     @Override
@@ -156,7 +156,7 @@ public class MenuClient {
     }
     /* Menu modification equipe */
     private Option modifyCompetition(Competition competition) {
-        Menu menu = new Menu("Menu | Modification competition : " + competition.getNom());
+        Menu menu = new Menu("Menu Modification competition : " + competition.getNom());
         menu.add(changerNomCompetition(competition));
         menu.add(changerDateCompetition(competition));
 
@@ -165,18 +165,18 @@ public class MenuClient {
         }else{
             menu.add(addPersonToCompetition(competition));
         }
-        menu.addBack("b");
+        menu.addBack("x");
 
         return menu;
     }
     /* changer nom competitions */
     private Option changerNomCompetition(Competition competition) {
         return new Option(
-                "Changer le nom de la compétition", "1",
+                "Changer nom competition", "1",
                 new Action() {
                     @Override
                     public void optionSelected() {
-                        competition.setNom(InOut.getString("Nouveau nom de la compétition : "));
+                        competition.setNom(InOut.getString("Nouveau nom competition : "));
                     }
                 }
         );
@@ -184,15 +184,15 @@ public class MenuClient {
     /* changer date de fin competitions */
     private Option changerDateCompetition(Competition competition) {
         return new Option(
-                "Changer la date de fin de la compétition", "2",
+                "Changer date de fin competition", "2",
                 new Action() {
                     @Override
                     public void optionSelected() {
-                        System.out.println("Nouvelle fin de la compétition : ");
-                        int jour = InOut.getInt("  Jour (jj) : ");
-                        int mois = InOut.getInt("  Mois (mm) : ");
-                        int annee = InOut.getInt("  Année (yyyy) : ");
-                        LocalDate dateCloture = LocalDate.of(annee, mois, jour);
+                        System.out.println("Nouvelle fin competition : ");
+                        int day = InOut.getInt("  Jour (jj) : ");
+                        int month = InOut.getInt("  Mois (mm) : ");
+                        int year = InOut.getInt("  Année (yyyy) : ");
+                        LocalDate dateCloture = LocalDate.of(year, month, day);
                         competition.setDateCloture(dateCloture);
                     }
                 }
@@ -201,7 +201,7 @@ public class MenuClient {
     /* Ajout personne competitions */
     private Option addPersonToCompetition(Competition competition) {
         return new List<>(
-                "Ajouter une personne à la competition", "3",
+                "Ajouter personne à la competition", "3",
                 new ListData<Personne>()
                 {
                     @Override
@@ -222,7 +222,7 @@ public class MenuClient {
     /* Ajout equipe competitions */
     private Option addEquipeToCompetition(Competition competition) {
         return new List<>(
-                "Ajouter une équipe à la competition", "4",
+                "Ajouter équipe à la competition", "4",
                 new ListData<Equipe>()
                 {
                     @Override
@@ -245,16 +245,16 @@ public class MenuClient {
      */
 
     private Menu equipeMenu() {
-        Menu menu = new Menu("Menu | Équipes", "Équipes", "e");
-        menu.add( addEquipeOption() );
-        menu.add( displayEquipeOption() );
-        menu.add( manageEquipeMenu() );
-        menu.addBack("b");
+        Menu menu = new Menu("Menu equipes", "equipes", "b");
+        menu.add( addEquipe() );
+        menu.add( displayEquipe() );
+        menu.add( manageEquipe() );
+        menu.addBack("x");
         return menu;
     }
     /* Afficher equipe */
-    private Option displayEquipeOption() { return new Option(
-            "Afficher les équipes", "2",
+    private Option displayEquipe() { return new Option(
+            "Afficher equipes", "2",
             new Action() {
                 @Override
                 public void optionSelected() {
@@ -263,22 +263,22 @@ public class MenuClient {
             });
     }
     /* Ajout equipe */
-    private Option addEquipeOption() { return new Option(
-            "Ajouter une équipe", "1",
+    private Option addEquipe() { return new Option(
+            "Ajouter equipe", "1",
             new Action()
             {
                 @Override
                 public void optionSelected()
                 {
-                    String a = InOut.getString("Nom de l'équipe: ");
+                    String a = InOut.getString("Nom equipe: ");
                     Inscriptions.getInscriptions().createEquipe(a);
                 }
             }
     ); }
     /* Supprimer equipe */
-    private Option deleteEquipeOption() {
+    private Option deleteEquipe() {
         return new List<>(
-                "Supprimer une équipe", "1",
+                "Supprimer equipe", "1",
                 new ListData<Equipe>()
                 {
                     @Override
@@ -297,17 +297,17 @@ public class MenuClient {
         );
     }
     /* Gestion equipe */
-    private Menu manageEquipeMenu() {
-        Menu menu = new Menu("Menu | Gestion des équipes", "Gestion des équipes","3");
-        menu.add( deleteEquipeOption() );
-        menu.add( modifyEquipeOption() );
-        menu.addBack("b");
+    private Menu manageEquipe() {
+        Menu menu = new Menu("Menu Gestion equipes", "Gestion equipes","3");
+        menu.add( deleteEquipe() );
+        menu.add( modifyEquipe() );
+        menu.addBack("x");
         return menu;
     }
     /* Modifier equipe */
-    private Option modifyEquipeOption() {
+    private Option modifyEquipe() {
         return new List<>(
-                "Modifier une équipe", "2",
+                "Modifier equipe", "2",
                 new ListData<Equipe>(){
 
                     @Override
@@ -326,30 +326,30 @@ public class MenuClient {
     }
     /* Menu modification equipe */
     private Option modifyEquipe(Equipe equipe) {
-        Menu menu = new Menu("Menu | Modification equipe : " + equipe.getNom());
+        Menu menu = new Menu("Menu Modification equipe : " + equipe.getNom());
         menu.add(changerNomEquipe(equipe));
-        menu.add(addPersonToEquipeOption(equipe));
-        menu.add(removePersonToEquipeOption(equipe));
-        menu.addBack("b");
+        menu.add(addPersonToEquipe(equipe));
+        menu.add(removePersonToEquipe(equipe));
+        menu.addBack("x");
 
         return menu;
     }
     /* Changer nom equipe */
     private Option changerNomEquipe(Equipe equipe) {
         return new Option(
-                "Changer le nom de l'équipe", "1",
+                "Changer nom equipe", "1",
                 new Action() {
                     @Override
                     public void optionSelected() {
-                        equipe.setNom(InOut.getString("Nouveau nom de l'équipe : "));
+                        equipe.setNom(InOut.getString("Nouveau nom equipe : "));
                     }
                 }
         );
     }
     /* Ajout personne equipe */
-    private Option addPersonToEquipeOption(Equipe equipe) {
+    private Option addPersonToEquipe(Equipe equipe) {
         return new List<>(
-                "Ajouter une personne à l'équipe", "2",
+                "Ajouter personne  a l'equipe", "2",
                 new ListData<Personne>()
                 {
                     @Override
@@ -368,9 +368,9 @@ public class MenuClient {
         );
     }
     /* Supprimer personne equipe */
-    private Option removePersonToEquipeOption(Equipe equipe) {
+    private Option removePersonToEquipe(Equipe equipe) {
         return new List<>(
-                "Supprimer une personne de l'équipe", "3",
+                "Supprimer personne de l'equipe", "3",
                 new ListData<Personne>()
                 {
                     @Override
@@ -392,16 +392,16 @@ public class MenuClient {
     /* Menu pour les personnes */
 
     private Menu personneMenu() {
-        Menu menu = new Menu("Menu | Menu des personnes", "Personnes", "p");
-        menu.add( addPersonneOption() );
-        menu.add( displayPersonneOption() );
-        menu.add( managePersMenu() );
-        menu.addBack("b");
+        Menu menu = new Menu("Menu Menu personnes", "Personnes", "c");
+        menu.add( addPersonne() );
+        menu.add( displayPersonne() );
+        menu.add( managePers() );
+        menu.addBack("x");
         return menu;
     }
     /* Afficher personne */
-    private Option displayPersonneOption() { return new Option(
-            "Afficher les personnes", "2",
+    private Option displayPersonne() { return new Option(
+            "Afficher personnes", "2",
             new Action() {
                 @Override
                 public void optionSelected() {
@@ -411,24 +411,24 @@ public class MenuClient {
     );
     }
     /* Ajout personne */
-    private Option addPersonneOption() {
-        return new Option("Ajouter une personne", "1",
+    private Option addPersonne() {
+        return new Option("Ajouter personne", "1",
                 new Action() {
                     @Override
                     public void optionSelected()
                     {
-                        String prenom = InOut.getString("Prénom de la personne: ");
-                        String nom = InOut.getString("Nom de la personne: ");
-                        String email = InOut.getString("Adresse e-mail de la personne: ");
-                        Inscriptions.getInscriptions().createPersonne(nom, prenom, email);
+                        String firstname = InOut.getString("Prénom : ");
+                        String name = InOut.getString("Nom : ");
+                        String email = InOut.getString("Adresse e-mail : ");
+                        Inscriptions.getInscriptions().createPersonne(name, firstname, email);
                     }
                 }
         );
     }
     /* Supprimer personne */
-    private Option deletePersonneOption() {
+    private Option deletePersonne() {
         return new List<>(
-                "Supprimer une personne", "1",
+                "Supprimer personne", "1",
                 new ListData<Personne>(){
 
                     @Override
@@ -447,17 +447,17 @@ public class MenuClient {
         );
     }
     /* Gestion personne */
-    private Menu managePersMenu() {
-        Menu menu = new Menu("Menu | Gestion des personnes", "Gestion des personnes","3");
-        menu.add( deletePersonneOption() );
-        menu.add( modifyPersonneOption() );
-        menu.addBack("b");
+    private Menu managePers() {
+        Menu menu = new Menu("Menu Gestion personnes", "Gestion personnes","3");
+        menu.add( deletePersonne() );
+        menu.add( modifyPersonne() );
+        menu.addBack("x");
         return menu;
     }
     /* Modifier personne */
-    private Option modifyPersonneOption() {
+    private Option modifyPersonne() {
         return new List<>(
-            "Modifier une personne", "2",
+            "Modifier personne", "2",
             new ListData<Personne>(){
 
                 @Override
@@ -476,18 +476,18 @@ public class MenuClient {
     }
     /* menu modification personne */
     private Option modifyPersonne(Personne personne) {
-        Menu menu = new Menu("Menu | Modifier une personne :  " + personne.getNom());
-        menu.add(changerNom(personne));
-        menu.add(changerPrenom(personne));
+        Menu menu = new Menu("Menu Modif personne :  " + personne.getNom());
+        menu.add(changerName(personne));
+        menu.add(changerFirstname(personne));
         menu.add(changerMail(personne));
-        menu.addBack("b");
+        menu.addBack("x");
 
         return menu;
     }
     /* Changer nom personne */
-    private Option changerNom(Personne personne) {
+    private Option changerName(Personne personne) {
         return new Option(
-                "Changer le nom", "1",
+                "Changer nom", "1",
                 new Action() {
                     @Override
                     public void optionSelected() {
@@ -497,9 +497,9 @@ public class MenuClient {
         );
     }
     /* Changer prenom personne */
-    private Option changerPrenom(Personne personne) {
+    private Option changerFirstname(Personne personne) {
         return new Option(
-                "Changer le prénom", "2",
+                "Changer prénom", "2",
                 new Action() {
                     @Override
                     public void optionSelected() {
@@ -515,7 +515,7 @@ public class MenuClient {
                 new Action() {
                     @Override
                     public void optionSelected() {
-                        personne.setMail(InOut.getString("Nouvelle adresse email : "));
+                        personne.setMail(InOut.getString("Nouveau email : "));
                     }
                 }
         );
